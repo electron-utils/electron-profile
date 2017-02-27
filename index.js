375553289931d2ccb6545d9cd8fde075278bd655
Johnny Wu <KarasAya@gmail.com>
2017-02-27 10:54:21 +0800

    fix unique version not work

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
 'use strict';
 
 const platform = require('electron-platform');
+const pkgJson = require('./package.json');
 
 let profile;
+let name = `__${pkgJson.name}__`;
+let msg = `Failed to require ${pkgJson.name}@${pkgJson.version}:
+  A different version of ${pkgJson.name} already running in the process, we will redirect to it.
+  Please make sure your dependencies use the same version of ${pkgJson.name}.`;
 
 if ( platform.isMainProcess ) {
-  const {app} = require('electron');
-  const path = require('path');
-  const fsJetpack = require('fs-jetpack');
+  if (global[name]) {
+    console.warn(msg);
+    profile = global[name];
+  } else {
+    const {app} = require('electron');
+    const path = require('path');
+    const fsJetpack = require('fs-jetpack');
 
-  profile = require('./lib/main');
+    profile = global[name] = require('./lib/main');
 
-  let home = path.join(app.getPath('home'), `.${app.getName()}`);
-  let local = path.join(home, 'local');
+    //
+    let home = path.join(app.getPath('home'), `.${app.getName()}`);
+    let local = path.join(home, 'local');
 
-  // ensure directories exists
-  // MacOSX: ~/Library/Logs/.${app-name}
-  // Windows: %APPDATA%, some where like 'C:\Users\${user_name}\AppData\Local\...'
-  fsJetpack.dir(home);
-  fsJetpack.dir(local);
+    // ensure directories exists
+    // MacOSX: ~/Library/Logs/.${app-name}
+    // Windows: %APPDATA%, some where like 'C:\Users\${user_name}\AppData\Local\...'
+    fsJetpack.dir(home);
+    fsJetpack.dir(local);
 
-  profile.register('global', home);
-  profile.register('local', local);
+    profile.register('global', home);
+    profile.register('local', local);
+  }
 } else {
-  profile = require('./lib/renderer/index');
+  if (window[name]) {
+    console.warn(msg);
+    profile = window[name];
+  } else {
+    profile = window[name] = require('./lib/renderer/index');
+  }
 }
 
 // ==========================
